ddc42bb5e82fd88f7e9849840ec114213d8c4171
Eric <eric.t4ng@gmail.com>
2018-02-05 16:40:04 +0000

    added button listener

--- app/src/main/java/com/example/eric/strokevictimdrivingtest/StartMenu.java
+++ app/src/main/java/com/example/eric/strokevictimdrivingtest/StartMenu.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
 
 public class StartMenu extends AppCompatActivity {
 
@@ -16,6 +18,7 @@ public class StartMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button start_button = (Button) findViewById(R.id.btnStarttest);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -24,6 +27,14 @@ public class StartMenu extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent myIntent = new Intent(StartMenu.this, );
+//                startActivity(myIntent);
             }
         });
     }
@@ -49,4 +60,6 @@ public class StartMenu extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
