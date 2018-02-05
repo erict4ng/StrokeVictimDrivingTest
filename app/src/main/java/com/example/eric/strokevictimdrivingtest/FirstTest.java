3cae9ef51068eaa5a9beb0fd00aa3cf9ded6e96b
amber <emootchi18@gmail.com>
2018-02-05 21:38:31 +0000

    Added timer and got an image to move when you drag it

--- app/src/main/java/com/example/eric/strokevictimdrivingtest/FirstTest.java
+++ app/src/main/java/com/example/eric/strokevictimdrivingtest/FirstTest.java
@@ -1,13 +1,56 @@
 package com.example.eric.strokevictimdrivingtest;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FirstTest extends AppCompatActivity {
+
+    private float xCoOrdinate, yCoOrdinate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_test);
+
+        final TextView timerText = (TextView)findViewById(R.id.timerTxt);
+
+        new CountDownTimer(900000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timerText.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                timerText.setText("done!");
+            }
+        }.start();
+
+        ImageView compass = (ImageView)findViewById(R.id.compassImage);
+        compass.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        xCoOrdinate = view.getX() - event.getRawX();
+                        yCoOrdinate = view.getY() - event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        view.animate().x(event.getRawX() + xCoOrdinate).y(event.getRawY() + yCoOrdinate).setDuration(0).start();
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
+            }
+        });
     }
+
+
+
 }
