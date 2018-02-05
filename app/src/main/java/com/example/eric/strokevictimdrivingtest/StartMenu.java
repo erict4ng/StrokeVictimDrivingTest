552f0ad77a4b088daf615394793f26f57f3a8fab
Eric <eric.t4ng@gmail.com>
2018-02-05 17:19:07 +0000

    removed event listeners and fab, replaced event listeners with onclick

--- app/src/main/java/com/example/eric/strokevictimdrivingtest/StartMenu.java
+++ app/src/main/java/com/example/eric/strokevictimdrivingtest/StartMenu.java
@@ -1,14 +1,11 @@
 package com.example.eric.strokevictimdrivingtest;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.content.Intent;
 
 public class StartMenu extends AppCompatActivity {
@@ -18,25 +15,7 @@ public class StartMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        Button start_button = (Button) findViewById(R.id.btnStarttest);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        start_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Intent myIntent = new Intent(StartMenu.this, );
-//                startActivity(myIntent);
-            }
-        });
     }
 
     @Override
@@ -61,5 +40,7 @@ public class StartMenu extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void start_test(View view) {
 
+    }
 }
