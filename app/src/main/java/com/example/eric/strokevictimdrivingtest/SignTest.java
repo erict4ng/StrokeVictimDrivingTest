7c778b4f113e1b09145a95ac3165c549ccf4a281
Eric <eric.t4ng@gmail.com>
2018-02-15 19:29:14 +0000

    added button to sign test

--- app/src/main/java/com/example/eric/strokevictimdrivingtest/SignTest.java
+++ app/src/main/java/com/example/eric/strokevictimdrivingtest/SignTest.java
@@ -193,6 +193,7 @@ public class SignTest extends AppCompatActivity {
         Intent intent = new Intent(this, DirectionsMatrixTest.class);
         intent.putExtras(bundle);
 
+
     startActivity(intent);
     }
 }
